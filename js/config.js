68d6bb6e87e8eccb4e7bfc3b22d9c4ab6a79f6f3
LuciferKilla <76596906+LuciferKilla@users.noreply.github.com>
2020-12-25 22:13:52 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,42 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'LuciferKilla',
+
+  // Социальные сети
+  social: {
+    telegram: '@Lucifer Killa',
+    steam: 'luciferterible',
+    vk: 'lucifer.killa', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#f5323c',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#000000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+ 
+
+  // Цвет иконок социальных сетей
+  socialColor: '#000000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
